d5f184985807f87ab371ce33709f30bbb03d7be1
Silvano di Ninno <silvano.dininno@nxp.com>
2016-12-21 13:17:00 +0100

    Format UUID strings correctly

--- ta/ocotp/ta/Android.mk
+++ ta/ocotp/ta/Android.mk
@@ -1,4 +1,4 @@
 LOCAL_PATH := $(call my-dir)
 
-local_module := cb356460-3789-11e6-8bdf0002a5d5c51b.ta
+local_module := cb356460-3789-11e6-8bdf-0002a5d5c51b.ta
 include $(BUILD_OPTEE_MK)
--- ta/ocram/ta-failure-ocram-memory/Android.mk
+++ ta/ocram/ta-failure-ocram-memory/Android.mk
@@ -1,4 +1,4 @@
 LOCAL_PATH := $(call my-dir)
 
-local_module := d1578b46-53b0-11e6-beb89e71128cae77.ta
+local_module := d1578b46-53b0-11e6-beb8-9e71128cae77.ta
 include $(BUILD_OPTEE_MK)
