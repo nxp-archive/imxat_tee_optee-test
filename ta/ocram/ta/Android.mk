7ad78f7a3edf8b2b11a7a5dee6a17def76a17d91
Silvano di Ninno <silvano.dininno@nxp.com>
2016-12-21 13:17:00 +0100

    Format UUID strings correctly

--- ta/ocram/ta/Android.mk
+++ ta/ocram/ta/Android.mk
@@ -1,4 +1,4 @@
 LOCAL_PATH := $(call my-dir)
 
-local_module := d7bd7ac0-3215-11e6-adcc0002a5d5c51b.ta
+local_module := d7bd7ac0-3215-11e6-adcc-0002a5d5c51b.ta
 include $(BUILD_OPTEE_MK)
--- ta/time_api/ta/Android.mk
+++ ta/time_api/ta/Android.mk
@@ -1,4 +1,4 @@
 LOCAL_PATH := $(call my-dir)
 
-local_module := 6f04bde0-2301-11e6-95e80002a5d5c51b.ta
+local_module := 6f04bde0-2301-11e6-95e8-0002a5d5c51b.ta
 include $(BUILD_OPTEE_MK)
